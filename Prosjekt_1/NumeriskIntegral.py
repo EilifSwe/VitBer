1ea5f51237cd31652f4aee038c05d89bd7890e28
bjornulfb <b.brekke96@hotmail.com>
2017-02-09 15:53:29 +0100

    Update NumeriskIntegral.py

--- Prosjekt_1/NumeriskIntegral.py
+++ Prosjekt_1/NumeriskIntegral.py
@@ -1,12 +1,9 @@
-import numpy as np
-import Parametere as par
-
 #Bruker trapesmetoden. x er en liste med N punkter. y er U-matrisen.
 def trapezoid(x, y):
     N = len(x)
     I = 0
     for i in range(0, N-1):
-        I += 0.5*(y[i]+y[i+1])*(x[i+1]- x[i])
+        I += 0.5*(y[i]+y[i+1])*(x[i+1]- x[i]) #Kjent uttrykk for trapes
     return I
 
 #Til å finne gjennomsnittsverdien til funksjonen(snittemperatur)
@@ -14,5 +11,3 @@ def average(x, y):
     N = len(x)
     intervall = x[N-1] - x[0]
     return trapezoid(x, y) / intervall
-    
-    
