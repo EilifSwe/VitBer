fffca063638a4e76830a5ab81d6f18dd62378446
jroeste <julir@stud.ntnu.no>
2017-02-09 15:51:51 +0100

    Update Parametere.py

--- Prosjekt_1/Parametere.py
+++ Prosjekt_1/Parametere.py
@@ -1,25 +1,23 @@
 #Variabler
 import numpy as np
 
-#Del1
+#Del 1: Parametere for å verifisere koden. 
 '''
 a=0 #Startpunkt
 b=1  #Sluttpunkt
-ua=1 #a Temp
-ub=np.e #b Temp
+ua=1 #a-temp
+ub=np.e #b-temp
 '''
 
-#Del2
-
+#Del 2: Parametere for å kjøre koden og finne sigma.
 a=0 #Startpunkt
 b=10  #Sluttpunkt
 ua=-1 #a Temp
 ub=1 #b Temp
 
-
 N=40  #Antall punkter
 my=2
-sig1=0.5
-sig2=10
-TOL = 0.000001
-U_AVG = 3
+sig1=0.5 #gjetter på startintervall
+sig2=10 
+TOL = 0.000001 #Toleransen for sigma.
+U_AVG = 3 #Gjennomsnitts-U.
