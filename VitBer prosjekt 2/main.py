468d7ebd4c83defd96068ee075dd9a2afdd07bef
EilifSwe <eilifs@hotmail.com>
2017-03-01 16:06:35 +0100

    Add files via upload

--- VitBer prosjekt 2/main.py
+++ VitBer prosjekt 2/main.py
@@ -11,25 +11,26 @@ def makeAMatrix(N, beta): #beta er liste med betaverdier og N er antall kroker
         j = (i + 1) % N
         #Venstre blokk
         A[4*i][4*i]=6
-        A[4*i][4*i+3]=beta[i]
+        A[4*i+1][4*i]=6
         A[4*i+1][4*i+1]=2
+        A[4*i+2][4*i]=3
+        A[4*i+2][4*i+1]=2
         A[4*i+2][4*i+2]=1
+        A[4*i+3][4*i]=1
+        A[4*i+3][4*i+1]=1
+        A[4*i+3][4*i+2]=1
         A[4*i+3][4*i+3]=1
+        
         #Høyre blokk
         A[4*i][4*j]=-6
-        A[4*i+1][4*j]=-6
+        A[4*i][4*j+3]=-beta[j]
         A[4*i+1][4*j+1]=-2
-        A[4*i+2][4*j]=-3
-        A[4*i+2][4*j+1]=-2
         A[4*i+2][4*j+2]=-1
-        A[4*i+3][4*j]=-1
-        A[4*i+3][4*j+1]=-1
-        A[4*i+3][4*j+2]=-1
         A[4*i+3][4*j+3]=-1
     return A
 
 def makeUVector(N,a): #N antall kroker, a = alfa
-    return np.array([-a,-0.5*a,-a/6,-a/24]*N)
+    return np.array([a,0.5*a,a/6,a/24]*N)
     
 def calculateXVector(A, U):
     return np.linalg.solve(A, U)
@@ -46,6 +47,7 @@ def updateAMatrix(beta,A):
 def eta(ksi, xArray, alpha):
     k = int(np.floor(ksi))
     x = ksi - k
+    
     return (((-(alpha / 24.) *x + xArray[4*k])*x + xArray[4*k+1])*x + xArray[4*k+2])*x + xArray[4*k+3]
 
 def dEta(ksi, xArray, alpha):
@@ -61,10 +63,11 @@ def ddEta(ksi, xArray, alpha):
 def dddEta(ksi, xArray, alpha):
     k = int(np.floor(ksi))
     x = ksi - k
+    
     return -alpha*x + 6*xArray[4*k]
     
 def plotEta(nr, N, xArray, alpha):
-    ksi = np.linspace(0,(N- 1), 200)
+    ksi = np.linspace(0, N - 0.001, 1000)
     etaArray = []
     
     func = None
@@ -89,14 +92,16 @@ def plotEta(nr, N, xArray, alpha):
 def main():
     N = 10
     beta=53.05*np.ones(N)
-    alpha = 1.061
+    beta[4] = 30
+    print(beta)
+    alpha = 10
     
     A = makeAMatrix(N, beta)
     printMatrix(A)
     U = makeUVector(N, alpha)
 
     X = calculateXVector(A, U)
-    
+    print(X)
     plotEta(0, N, X, alpha)
     
 main()
