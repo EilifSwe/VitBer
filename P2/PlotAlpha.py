971c03c1627e377a2d4c53fa814221cd526d9090
jroeste <julir@stud.ntnu.no>
2017-03-07 20:14:08 +0100

    Update PlotAlpha.py

--- P2/PlotAlpha.py
+++ P2/PlotAlpha.py
@@ -23,36 +23,39 @@ def make_t_list(N, limit):
     for i in range(len(t_list)):
         t_list[i]=r.random()*limit
     return t_list
-    
+
+##---Riktig versjon:
 def make_alpha_list(N, M, beta):
     betaList = np.zeros(N)
     A = LS.makeAMatrix(N, betaList)
     U = LS.makeUVector(N, 1)
     
-    betaIndexList = np.zeros(N)
+    #betaIndexList = np.zeros(N)
+    betaIndexMatrix=np.zeros([M,N]) #lager en matrise for å få til fargeplottet
     alpha = np.zeros(N)
     
-    
     for i in range(M):
-        betaList = beta*np.ones(N)
-        LS.updateAMatrix(betaList, A, N)
-        tList = make_t_list(N, SC.maxThreshold)
-        for k in range(0, N):
-            xRef = LS.calculateXVector(A, U)
-            betaIndex, rValue = findNextBreak(tList, betaList, xRef)
-            betaIndexList[k] = betaIndex
+        betaList = beta*np.ones(N) #ok
+        LS.updateAMatrix(betaList, A, N) #ok
+        tList = make_t_list(N, SC.maxThreshold) #ok
+        for k in range(0, N): #ok
+            xRef = LS.calculateXVector(A, U) #ok
+            betaIndex, rValue = findNextBreak(tList, betaList, xRef) 
+            #betaIndexList[k] = betaIndex
+            betaIndexMatrix[i,k]=betaIndex
             alpha[k] += betaList[betaIndex] / rValue
             betaList[betaIndex] = 0
-            LS.updateAMatrix(betaList, A, N)
+            LS.updateAMatrix(betaList, A, N) #hvorfor er det forskjell her og i make_alpha_sparse?
     alpha = alpha/M
-    return alpha, betaIndexList
-    
+    return alpha, betaIndexMatrix #betaIndexList
+##----
 def make_alpha_list_sparse(N, M, beta):
     betaList = np.zeros(N)
     A = LS.makeSparseAMatrix(N, betaList)
     U = LS.makeUVector(N, 1)
     
-    betaIndexList = np.zeros(N)
+    #betaIndexList = np.zeros(N)
+    betaIndexMatrix=np.zeros([M,N]) #matrise for å få til fargeplott
     alpha = np.zeros(N)
     
     
@@ -63,24 +66,37 @@ def make_alpha_list_sparse(N, M, beta):
         for k in range(0, N):
             xRef = LS.calculateSparseXVector(A, U)
             betaIndex, rValue = findNextBreak(tList, betaList, xRef)
-            betaIndexList[k] = betaIndex
+            #betaIndexList[k] = betaIndex
+            betaIndexMatrix[i,k]=betaIndex #legger beta-indeksene i matrisa for hver iterasjon
             alpha[k] += betaList[betaIndex] / rValue
             betaList[betaIndex] = 0
-            A[4*betaIndex, 4*betaIndex+3]=-betaList[betaIndex]
+            A[4*betaIndex, 4*betaIndex+3]=-betaList[betaIndex] #hvorfor forskjell her og i vanlig?
     alpha = alpha/M
-    return alpha, betaIndexList
-    
-def main(numberOfIterations, numberOfSprings, beta, sparse):
+    return alpha, betaIndexMatrix #betaIndexList
+
+def main(numberOfIterations, numberOfSprings,beta,sparse):
+   ##----------------------
     M = numberOfIterations
     N = numberOfSprings
     iteration = np.linspace(0,1,N)
     if sparse:
-        alphaList, betaIndexList = make_alpha_list_sparse(N, M, beta)
+        alphaList, betaIndexMatrix = make_alpha_list_sparse(N, M, beta) #byttet ut list med matrix
     else:
-        alphaList, betaIndexList = make_alpha_list(N, M, beta)
+        alphaList, betaIndexMatrix = make_alpha_list(N, M, beta) #byttet ut list med Matrix
     
-    plt.plot(iteration, betaIndexList, 'o')
     plt.figure()
+    plt.matshow(betaIndexMatrix) #måte å plotte fargeplottet
+    plt.xlabel("n",fontsize=15)
+    plt.ylabel("Antall simuleringer",fontsize=15)
+    plt.rcParams['xtick.labelsize'] = 15
+    plt.rcParams['ytick.labelsize'] = 15
+    plt.savefig("simuleringer_vitber2.pdf")
     
+    plt.figure()
     plt.plot(iteration, alphaList)
-    plt.show()
+    plt.xlabel("n/N Antall iterasjoner",fontsize=15)
+    plt.ylabel("Midlere $\\alpha$",fontsize=15)
+    plt.rcParams['xtick.labelsize'] = 15
+    plt.rcParams['ytick.labelsize'] = 15
+    plt.savefig("vitber_alpha_prosj2.pdf")
+    plt.show()    
