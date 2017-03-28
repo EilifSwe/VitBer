8593b8c26cc8a4f1b63e9650bac7e13650c33a01
jroeste <julir@stud.ntnu.no>
2017-03-28 13:52:40 +0200

    Add files via upload

--- P3/DiffSolver.py
+++ P3/DiffSolver.py
@@ -25,11 +25,10 @@ def trapezoid(N, T, t0, X0, f):
         X[i+1] = trapezoidStep(i*dt, dt, X[i], f)
     return X
     
-def varTimeTrapezoidStep(x, L, y, f, TOL):
-    k0 = f(x,y)
+def varTimeTrapezoidStep(x, L, y, f, TOL,delX):
+    k0 = f(x,y) #stigning  i startpunkt
     
-    delX = L/100
-    fk1 = f(x+delX, y + delX*k0)
+    fk1 = f(x+delX, y + delX*k0) 
     dx = 0.8*np.sqrt((2*TOL)*delX/np.abs(fk1-k0))
     
     if(x+dx >L):
@@ -39,9 +38,14 @@ def varTimeTrapezoidStep(x, L, y, f, TOL):
     while(0.5*dx*abs(k1-k0)>TOL):
         dx=dx/2
         k1 = f(x +dx, y + dx*k0)
-    
+    if (0.5*dx*abs(k1-k0)<0.1*TOL):
+        dx=2*dx
     print(0.5*dx*abs(k1-k0)/TOL)
-    return x + dx, y + 0.5*dx*(k0+k1)
+    newpoint=x + dx, y + 0.5*dx*(k0+k1)
+    if (0.5*dx*abs(k1-k0)<0.3*TOL):
+        dx=2*dx
+        #print("hurra :D")
+    return newpoint,dx
     
 def varTimeTrapezoid(L, x0, y0, f, TOL):
     maxN = 100
@@ -49,30 +53,31 @@ def varTimeTrapezoid(L, x0, y0, f, TOL):
     y = np.zeros(maxN)
     y[0] = y0
     x[0] = x0
+    delX=L/100
     for i in range(0, maxN-1):
-        x[i+1], y[i+1] = varTimeTrapezoidStep(x[i], L, y[i], f, TOL)
+        (x[i+1], y[i+1]),delX= varTimeTrapezoidStep(x[i], L, y[i], f, TOL,delX)
         if(x[i+1] >= L):
             break
     return x, y
     
-def func(x,y):
+def func(x,y): #den deriverte
     return np.cos(x)
-
 
 def main():
     N = 100
-    L = 2*np.pi
-    TOL=0.01
+    L = 4*np.pi
+    TOL=0.02
     
     xAn = np.linspace(0,L,200)
     yAn = np.sin(xAn)
+    #yAn=np.tan(xAn)
     
     xVTr, yVTr = varTimeTrapezoid(L, 0, 0, func, TOL)
-    plt.figure(figsize=(10,15))
+    plt.figure(figsize=(15,8))
     p1, = plt.plot(xAn, yAn, label="Analytic")
    # p2, = plt.plot(xFE, yFE, label="Forward Euler")
     #p3, = plt.plot(xTr, yTr, label="Trapezoid")
-    p4, = plt.plot(xVTr, np.zeros(N),'ro', label="Variable Trapezoid")
+    p4, = plt.plot(xVTr, yVTr,'ro', label="Variable Trapezoid")
     plt.legend(loc = 0,handles=[p1,p4])
     plt.show()
     plt.show()
