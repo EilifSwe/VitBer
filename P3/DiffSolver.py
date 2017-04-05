b9000377d3e4881dcb751198a202768c803e5215
jroeste <julir@stud.ntnu.no>
2017-04-06 00:06:43 +0200

    Update DiffSolver.py

--- P3/DiffSolver.py
+++ P3/DiffSolver.py
@@ -7,13 +7,13 @@ def forwardEulerStep(t, dt, X, f):
 def forwardEuler(dt, T, t0, X0, f):
     N=T/dt
     N_int=int(N)
-    if N!=N_int:
-        N_int=N_int+1
+    if N!=N_int:        #Hvis N er et desimaltall runder int(N) tallet ned, 
+        N_int=N_int+1   #og det vil trengs et ekstra steg.
     X = np.asarray([np.zeros(2) for i in range(N_int+1)])        
     X[0] = X0
     for i in range(0, N_int):
-        if i==N_int-1:
-            dt=min(dt,T-i*dt)
+        if i==N_int-1:          #Ved det siste steget velges det korteste av
+            dt=min(dt,T-i*dt)   #tidssteget dt og differansen mellom totaltid og brukt tid.
         X[i+1] = forwardEulerStep(i*dt, dt, X[i], f)
     return X
     
@@ -25,25 +25,26 @@ def trapezoidStep(t, dt, X, f):
 def trapezoid(dt, T, t0, X0, f):
     N=T/dt
     N_int=int(N)
-    if N!=N_int:
-        N_int=N_int+1
+    if N!=N_int:        #Hvis N er et desimaltall runder int(N) tallet ned, 
+        N_int=N_int+1   #og det vil trengs et ekstra steg.
     X = np.asarray([np.zeros(2) for i in range(N_int+1)]) 
     X[0] = X0
     for i in range(0, N_int):
-        if i==N_int-1:
-            dt=min(dt,T-i*dt)
+        if i==N_int-1:          #Ved det siste steget velges det korteste av
+            dt=min(dt,T-i*dt)   #tidssteget dt og differansen mellom totaltid og brukt tid.
         X[i+1] = trapezoidStep(i*dt, dt, X[i], f)
     return X
-    
+
+#Oppgave 1d)
 def varTimeTrapezoidStep(t, X, f, TOL, dt, e, endt):
-    if(e < 0.5*TOL):
+    if(e < 0.5*TOL):    
         dt = 0.8*np.sqrt(TOL/e)*dt
     
     k0 = f(X,t)
     k1 = f(X+dt*k0, t + dt)
     
     while(True):
-        e = np.linalg.norm(k1-k0)*dt/2
+        e = np.linalg.norm(k1-k0)*dt/2  #linalg.norm sjekker XXXXX
         
         if(e < TOL):
             break
