82e04ba454d2fa5a24f31f7dd6d616b2fa0ee45c
EilifSwe <eilifs@hotmail.com>
2017-04-06 11:10:31 +0200

    Add files via upload

--- P3/Oppgave2.py
+++ P3/Oppgave2.py
@@ -17,17 +17,31 @@ import Oppgave1 as O1
 import DiffSolver as DS
 import Interpolator_Class as IC
 
-#Plotter en partikkels bane for 3 ulike tidspunkt, og 3 partikler på ulike steder til samme tid.
 def plotPath(map):
     totalTime = 10*24*3600
     h = 3600
-    xcoord=-3000000
-    ycoord=-1200000
-    X0 = np.array([xcoord, ycoord]).reshape(2,1)
+    xcoord0=-3000000
+    ycoord0=-1200000
+    xcoord1=-3000000
+    ycoord1=-1200000
+    xcoord2=-3000000
+    ycoord2=-1200000
+    
+    
     
     t0 = np.datetime64('2017-02-01T12:00:00')
     t1 = np.datetime64('2017-02-05T12:00:00')
     t2 = np.datetime64('2017-02-10T12:00:00')
+    
+    if(map):
+        xcoord0=-3000000
+        ycoord0=-1200000
+        xcoord1=-3000000
+        ycoord1=-1300000
+        xcoord2=-3000000
+        ycoord2=-1400000
+        t1 = np.datetime64('2017-02-01T12:00:00')
+        t2 = np.datetime64('2017-02-01T12:00:00')
     
     d  = xr.open_dataset('NorKyst-800m.nc')
     f  = IC.Interpolator(dataset=d)
@@ -40,9 +54,9 @@ def plotPath(map):
     X2 = np.asarray([np.zeros(2).reshape(2,1) for i in range(N+1)])
     X3 = np.asarray([np.zeros(2).reshape(2,1) for i in range(N+1)])
 
-    X1[0] = X0
-    X2[0] = X0
-    X3[0] = X0
+    X1[0] = np.array([xcoord0, ycoord0]).reshape(2,1)
+    X2[0] = np.array([xcoord1, ycoord1]).reshape(2,1)
+    X3[0] = np.array([xcoord2, ycoord2]).reshape(2,1)
     
     for i in range(0, N):
         print(i)
@@ -51,27 +65,27 @@ def plotPath(map):
         X3[i+1] = DS.trapezoidStep(t2 + i*dt, dt, X3[i], f)
         
     
-    if(map): #Plotter på kart med 3 ulike startposisjoner.
+    if(map):
         fig = plt.figure(figsize=(12,8))
         ax  = plt.axes(projection=ccrs.NorthPolarStereo())
         land_10m = cfeature.NaturalEarthFeature('physical', 'land', '10m', color = '#dddddd')
         ax.add_feature(land_10m)
         ax.coastlines(resolution='10m')
-        
-        #Transformerer fra x,y til lat,long:
-        p1 = pyproj.Proj(d.projection_stere.proj4) 
-        p2 = pyproj.Proj(proj='latlong')           
+        p1 = pyproj.Proj(d.projection_stere.proj4)
+        p2 = pyproj.Proj(proj='latlong')
         lons0, lats0 = pyproj.transform(p1, p2, X1[:,0,0], X1[:,1,0])
         lons1, lats1 = pyproj.transform(p1, p2, X2[:,0,0], X2[:,1,0])
         lons2, lats2 = pyproj.transform(p1, p2, X3[:,0,0], X3[:,1,0])
         
-        ax.plot(lons0, lats0, transform=ccrs.PlateCarree(), zorder=2,label="Posisjon 1: ",lons0,",",lats0)
-        ax.plot(lons1, lats1, transform=ccrs.PlateCarree(), zorder=2,label="Posisjon 2: ",lons1,",",lats1)
-        ax.plot(lons2, lats2, transform=ccrs.PlateCarree(), zorder=2,label="Posisjon 3: ",lons2,",",lats2)
-        ax.set_extent((-3, 7, 58, 62))      #kartutsnittets størrelse.
-        plt.savefig("BaneMap_oppg2a.pdf")
+        ax.plot(lons0, lats0, transform=ccrs.PlateCarree(), zorder=2)
+        ax.plot(lons1, lats1, transform=ccrs.PlateCarree(), zorder=2)
+        ax.plot(lons2, lats2, transform=ccrs.PlateCarree(), zorder=2)
         
-    else: #Partikler med lik startposisjon til forskjellig tid.
+        #ax.set_extent((-5, 15, 57, 67))
+        ax.set_extent((-3, 7, 58, 62))
+    
+        plt.savefig("BaneMap_oppg2a.pdf")
+    else:
         plt.figure()
         plt.plot(X1[:,0,0], X1[:,1,0],label="$t_0$ = 1. Feb")
         plt.plot(X2[:,0,0], X2[:,1,0],label="$t_0$= 5. Feb")
@@ -84,6 +98,11 @@ def plotPath(map):
         plt.legend(loc=2)
         
         plt.savefig("BaneNormal_oppg2a.pdf")
-
+        
+        
+   
+    
+    
     plt.style.use('bmh')
+    #plt.plot(X[:,0,0],X[:,1,0])
     plt.show()
