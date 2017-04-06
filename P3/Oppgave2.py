998f4586e23aba53e1e3a7c93aa91114ded9a7ce
jroeste <julir@stud.ntnu.no>
2017-04-06 15:09:03 +0200

    Add files via upload

--- P3/Oppgave2.py
+++ P3/Oppgave2.py
@@ -14,7 +14,8 @@ import DiffSolver as DS
 import Interpolator_Class as IC
 
 #Plotter en partikkels bane for 3 ulike tidspunkt, og 3 partikler på ulike steder til samme tid.
-def plotPath(map):
+def plotPath ( map ) :
+
     totalTime = 10*24*3600
     h = 3600
     xcoord0=-3000000
@@ -23,9 +24,7 @@ def plotPath(map):
     ycoord1=-1200000
     xcoord2=-3000000
     ycoord2=-1200000
-    
-    
-    
+
     t0 = np.datetime64('2017-02-01T12:00:00')
     t1 = np.datetime64('2017-02-05T12:00:00')
     t2 = np.datetime64('2017-02-10T12:00:00')
@@ -54,14 +53,14 @@ def plotPath(map):
     X1[0] = np.array([xcoord0, ycoord0]).reshape(2,1)
     X2[0] = np.array([xcoord1, ycoord1]).reshape(2,1)
     X3[0] = np.array([xcoord2, ycoord2]).reshape(2,1)
-    
+
     for i in range(0, N):
         print(i)
         X1[i+1] = DS.trapezoidStep(t0 + i*dt, dt, X1[i], f)
         X2[i+1] = DS.trapezoidStep(t1 + i*dt, dt, X2[i], f)
         X3[i+1] = DS.trapezoidStep(t2 + i*dt, dt, X3[i], f)
-        
-    
+
+
     if(map): #Plotter på kart med 3 ulike startposisjoner.
         fig = plt.figure(figsize=(12,8))
         ax  = plt.axes(projection=ccrs.NorthPolarStereo())
@@ -85,6 +84,7 @@ def plotPath(map):
 
     else:   #Partikler med lik startposisjon til forskjellig tid.
         plt.figure()
+        plt.plot(X1[0,0,0],X1[0,1,0],'x',c='g')
         plt.plot(X1[:,0,0], X1[:,1,0],label="$t_0$ = 1. Feb")
         plt.plot(X2[:,0,0], X2[:,1,0],label="$t_0$= 5. Feb")
         plt.plot(X3[:,0,0], X3[:,1,0],label="$t_0$= 10. Feb")
@@ -96,11 +96,6 @@ def plotPath(map):
         plt.legend(loc=2)
         
         plt.savefig("BaneNormal_oppg2a.pdf")
-        
-        
-   
-    
-    
+
     plt.style.use('bmh')
-    #plt.plot(X[:,0,0],X[:,1,0])
     plt.show()
